5741f971cc84e24adb48c89e933d17348e879c08
Kishanq048 <morrocoflyer824@gmail.com>
2025-03-05 22:19:52 +0530

    Update kernel.h

--- kernel.h
+++ kernel.h
@@ -2,46 +2,23 @@
 #define KERNEL_H
 
 typedef struct {
-	
-	float **data;
-	int size;
-	
+    float **data;
+    int size;
 } Kernel;
 
 typedef struct {
-
-	int **data;
-	int width;
-	int height;
-
-} Image;
-
-typedef struct {
-
-    int **input_image;
-    int **output_image;
-    Kernel k;
+    int **data;
     int width;
     int height;
-    int start_row;
-    int end_row;
-
-} ThreadData;
+} Image;
 
 Kernel getKernel(int size);
-
 void readKernel(Kernel k);
-
 Image convolution(int** input_image, Kernel k, int width, int height);
-
 Image _loadImage(int** _image, int width, int height);
-
 Image* addPadding(Image* input_image, int padding);
-
 void Release(int **array, int rows);
-
 Image read_image_from_file(const char *filename);
-
 Image mul_convolution(int** input_image, Kernel k, int width, int height);
 
 #endif
